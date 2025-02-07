c540eb401bb71ee078bad05195fe45ea0cddb283
rkhatta1 <rkhatta1@asu.edu>
2025-02-06 17:46:34 -0700

    Updated the actions files.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/SplitThatTemp",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
 
 export default nextConfig;
